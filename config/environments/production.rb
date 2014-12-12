eda1b575097931f72879d2f77fb7844ea8c0ef11
Dean Gibbons <dgibbons89@gmail.com>
2014-12-12 01:25:21 -0500

    credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,5 +80,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'https://afternoon-wave-3543.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://nameless-sea-8668.herokuapp.com/' }
 end
